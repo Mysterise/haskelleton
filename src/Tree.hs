70e74557559141eb91475d7ad06b2d72c4eec104
Tony Tang <tonytang.dev@gmail.com>
2020-02-09 02:14:48 +1100

    realised cant make monad instance for bsts with intermediate values, reordered Node val positions, aesthetic spacing

--- src/Tree.hs
+++ src/Tree.hs
@@ -1,15 +1,15 @@
 data Tree a = Nil
             | Leaf a
-            | Node (Tree a) a (Tree a)  
+            | Node a (Tree a) (Tree a)  
             deriving Show
 
 instance Functor Tree where
     fmap f (Nil)        = Nil
     fmap f (Leaf a)     = Leaf (f a)
-    fmap f (Node l a r) = Node (f <$> l) (f a) (f <$> r)
+    fmap f (Node a l r) = Node (f a) (f <$> l) (f <$> r)
 
 instance Applicative Tree where
-    pure                            = Leaf
-    Nil <*> _                       = Nil
-    (Leaf f) <*> t                  = f <$> t
-    (Node l f r) <*> (Node l' a r') = Node (l <*> l') (f a) (r <*> r') 
+    pure                                = Leaf
+    Nil             <*> _               = Nil
+    (Leaf f)        <*> t               = f <$> t
+    (Node f l r)    <*> (Node a l' r')  = Node (f a) (l <*> l') (r <*> r') 
